4adf72e727bf092e65d3de8aa194636694fbc27e
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-23 15:19:06 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import { Switch,Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (<div>
+    <Header/>
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
