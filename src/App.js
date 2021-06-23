ffd8f43bf5e700f1dfa47aa8d4847ed10b3a77db
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-23 15:00:19 +0530

    Collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch,Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (<div>
-      <HomePage/>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
+    </Switch>
      </div>
   );
 }
