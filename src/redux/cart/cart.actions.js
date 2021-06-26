64d59b8978d75b4d35499d00054178dc50d0b0e9
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-26 18:59:31 +0530

    cart changes

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import cartActionTypes from './cart.types';
 export const toggleCartHidden = () =>({
     type: cartActionTypes.TOGGLE_cART_HIDDEN
 });
+
+export const addItem = item => ({
+    type:cartActionTypes.ADD_ITEM,
+    payload:item
+})
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const cartActionTypes = {
-    TOGGLE_cART_HIDDEN : 'TOGGLE_cART_HIDDEN'
+    TOGGLE_cART_HIDDEN : 'TOGGLE_cART_HIDDEN',
+    ADD_ITEM : 'ADD_ITEM'
 }
 
 export default cartActionTypes;
