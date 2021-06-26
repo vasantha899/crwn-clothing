15e5f1264e459867369f79643ace89936d481d01
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-26 13:51:06 +0530

    Cart functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,10 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = () => (
+const Header = ({hidden}) => (
     <div className="header">
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -19,11 +21,15 @@ const Header = () => (
         <Link className='option' to="/signin">
             SIGN IN
         </Link>
+        <CartIcon />
         </div>
+        { hidden ? null : <CartDropDown/> }
     </div>
 );
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user :{currentUser},cart:{hidden} }) => ({
+    currentUser,
+    hidden
   });
+  
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const cartActionTypes = {
+    TOGGLE_cART_HIDDEN : 'TOGGLE_cART_HIDDEN'
+}
+
+export default cartActionTypes;
