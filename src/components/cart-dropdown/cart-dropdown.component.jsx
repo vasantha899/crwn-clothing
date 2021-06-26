d8a95aad2f54c79e0539c316f9df8864e2a06102
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-26 19:17:42 +0530

    car item in cart drop down

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
-
+import CartItem from '../cart-item/cart-item.component';
 
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      
+      {
+          cartItems.map(cartItem => (
+              <CartItem key = {CartItem.id} item={cartItem}/>
+          ))
+      }
     </div>
     <CustomButton
     >
@@ -19,4 +23,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart :{cartItems}}) => ({
+    cartItems
+})
+export default connect(mapStateToProps,null)(CartDropdown);
